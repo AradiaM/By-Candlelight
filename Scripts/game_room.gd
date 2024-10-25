34820934676a0675dcbe6b2988eb34b0f36fb4a1
Aradia Mauras <aradiamauraspr@gmail.com>
2024-10-24 23:58:28 -0400

    Latest

--- Scripts/game_room.gd
+++ Scripts/game_room.gd
@@ -1,10 +1,18 @@
+@tool
 extends PanelContainer
 class_name Game_Room
 
-@export var RoomName := "Room Name"
-@export var RoomDesc := "This is the description"
+@export var RoomName := "Room Name":set = set_zone_name 
+@export_multiline var RoomDesc := "This is the description" :set = set_zone_desc
 
 var exits: Dictionary = {}
+
+func set_zone_name(new_name: String):
+	$"MarginContainer/VBoxContainer/Room Name".text = new_name
+	RoomName = new_name
+func set_zone_desc(new_desc: String):
+	$"MarginContainer/VBoxContainer/Room Description".text = new_desc
+	RoomDesc = new_desc
 
 func connect_exit(direction: String, room):
 	match direction:
